f9b27aad465f05b189e10fcddd3640276345ed1b
root <3290@holbertonschool.com>
2022-04-28 17:34:31 +0000

    task-fixeada

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,10 +6,9 @@ def minOperations(n):
     """min operations func """
     minOP = 0
     div = 2
-    while isinstance(n, int):
-        if n > 1:
-            while n % div:
-                div += 1
-            minOP += div
-            n = int(n / div)
-        return minOP
+    while isinstance(n, int) and n > 1:
+        while n % div:
+            div += 1
+        minOP += div
+        n = int(n / div)
+    return minOP
